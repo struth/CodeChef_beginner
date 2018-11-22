9527a25151e15b689aabebeb3da4e962b265a183
struth <kuntesv@gmail.com>
2018-11-22 17:36:24 +0530

    Add files via upload

---
+++ Add Two Numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+  int t;
+  int a1,a2;
+
+  cin>>t;
+  while(t--)
+  {
+  cin>>a1>>a2;
+  cout<<a1+a2<<endl;
+  }
+  return 0;
+}
